16cca2d98a611f4a57f18d2c079022e182c21501
Malik Melih HORAN <info@malikmelih.com>
2023-01-26 01:15:13 +0300

    Delete app/src/main/java/com/example/depostok/db directory

--- app/src/main/java/com/example/depostok/db/Datas.java
+++
@@ -1,9 +0,0 @@
-package com.example.depostok.db;
-
-public class Datas{
-    String user_name_sname;
-
-    public String getUserName() { return user_name_sname; }
-
-    public String setUserName(String text) { user_name_sname = text; return user_name_sname; }
-}
--- app/src/main/java/com/example/depostok/db/UrlConnection.java
+++
@@ -1,6 +0,0 @@
-package com.example.depostok.db;
-
-public class UrlConnection {
-    public static final String LOGIN_URL = "https://malikmelih.com/inventory/";
-    public static final String DEVICE_URL = "https://malikmelih.com/inventory/data/";
-}
